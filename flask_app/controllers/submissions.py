f2fc21eb631b42865a9aa5f4b3f888e5b7f66117
WilRohde <warohde@gmail.com>
2022-03-19 20:05:30 -0400

    committing submission changes

--- flask_app/controllers/submissions.py
+++ flask_app/controllers/submissions.py
@@ -1,16 +1,21 @@
 from flask import session, render_template, request, redirect, flash
 from flask_app import app
 from flask_app.models.Submission import Submission
-from flask_app.models.Genre import Genre
-import os.path
+from flask_app.models.Group import Group
 
-@app.route('/submission/<int:id>')
-def edit_submission():
-    pass
+@app.route('/submit/review/<int:id>')
+def edit_submission(id):
+    data = {
+        'id': id
+    }
+    return render_template('review_submission.html', submission = Submission.get(data))
 
-@app.route('/submit/new')
-def new_submission():
-    return render_template('upload_submission.html')
+@app.route('/submit/new/<int:id>')
+def new_submission(id):
+    data = {
+        'id': id
+    }
+    return render_template('upload_submission.html', group = Group.get_by_id(data))
 
 @app.route('/submit/create')
 def create_submission():
@@ -23,20 +28,14 @@ def create_submission():
     Submission.save(data)
     return redirect('/dashboard')
 
-@app.route('/submit/read')
+@app.route('/submit/save', methods=['POST'])
 def upload_submission():
-    filename = request.form['filename']
-    file = open(filename) # (r)ead and (t)ext are default values
-    filedata = {
-        'filetype': os.path.splitext(request.form['filename'])[1],
-        'filesize': os.path.getsize(request.form['filename']),
+    data = {
+        'title': request.form['title'],
+        'description': request.form['description'],
+        'submission_text': request.form['submission_text'],
+        'group_id': request.form['group_id'],
+        'Author_id': session['Author_id']
     }
-    return render_template('create_submission.html',submission_text = file.read(), file_data = filedata)
-
-@app.route('/submit/manual')
-def manual_submission():
-    filedata = {
-        'filetype': "",
-        'filesize': ""
-    }
-    return render_template('create_submission.html', submission_text = "", file_data = filedata)
+    Submission.save(data)
+    return redirect('/dashboard')
