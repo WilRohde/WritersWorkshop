bd17edcec1ac497afc2464f259e2424f79c61321
WilRohde <warohde@gmail.com>
2022-05-12 08:22:11 -0400

    changes to db

--- server.py
+++ server.py
@@ -1,9 +1,4 @@
 from flask_app import app
-from flask_app.controllers.authors import Author
-from flask_app.controllers.groups import Group
-from flask_app.controllers.genres import Genre
-from flask_app.controllers.reviews import Review
-from flask_app.controllers.submissions import Submission
-
+from flask_app.controllers import authors, genres, groups, reviews, submissions
 if __name__=="__main__":
     app.run(debug=True)
